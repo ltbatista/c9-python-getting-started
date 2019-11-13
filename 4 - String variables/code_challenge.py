2b72d8d8e8e7e47c65864c1bc984a99cd7725031
ltbatista <ltbatista30@gmail.com>
2019-11-13 11:15:10 -0300

    Resolvendo o code_challenge de strings

--- 4 - String variables/code_challenge.py
+++ 4 - String variables/code_challenge.py
@@ -4,3 +4,12 @@
 # Make sure you have a space between first and last name
 # Make sure the first letter of first name and last name is uppercase
 # Make sure the rest of the name is lowercase
+
+fname = input("\nInput your first name: ")
+lname = input("Input your last name: ")
+
+print("\nYour name: {}".format(fname.capitalize() +" "+
+                                lname.capitalize()))
+
+
+
