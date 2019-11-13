f7b8fbd9172ffd455315f0bb155268ed4587755f
ltbatista <ltbatista30@gmail.com>
2019-11-13 13:14:14 -0300

    Code Challeng Numerico

--- 5 - Numeric variables/code_challenge.py
+++ 5 - Numeric variables/code_challenge.py
@@ -4,3 +4,11 @@
 # Print 'first number + second number = answer' 
 # For example if someone enters 4 and 6 the output should read
 # 4 + 6 = 10
+
+
+fnumber = input('\nEnter a number: ')
+snumber = input('Enter a second number: ')
+
+soma = int(fnumber) + int(snumber)
+
+print('{} + {} = {}'.format(fnumber,snumber,soma))
