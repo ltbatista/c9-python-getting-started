c5c8e3214d943718e4395d88062ca4a7dc3dbba9
ltbatista <ltbatista30@gmail.com>
2019-11-14 10:13:02 -0300

    Code Challenge - Date

--- 6 - Dates/code_challenge.py
+++ 6 - Dates/code_challenge.py
@@ -2,3 +2,20 @@
 # print yesterday's date
 # ask a user to enter a date
 # print the date one week from the date entered
+
+from datetime import datetime, timedelta
+
+today = datetime.now()
+
+print('Today is: {}'.format(str(today)))
+
+one_day = timedelta(days=1)
+yesterday = today - one_day
+print('Yesterday was: {}'.format(str(yesterday)))
+
+user_input = input('Input a date (dd/mm/yyyy): ')
+user_date = datetime.strptime(user_input, '%d/%m/%Y')
+
+one_week = timedelta(days=7)
+weekf_user = user_date + one_week
+print('The date one week from user inputed: {}'.format(weekf_user))
